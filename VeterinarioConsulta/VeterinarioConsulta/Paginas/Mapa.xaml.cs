d5f8e46624723a6fb665e73cef87bce50882bd33
RodrigoCesarAntunes <ro_sgca@hotmail.com>
2018-12-26 13:02:06 -0200

    Icones adicionados ao botões da tabbedPage

--- VeterinarioConsulta/VeterinarioConsulta/Paginas/Mapa.xaml.cs
+++ VeterinarioConsulta/VeterinarioConsulta/Paginas/Mapa.xaml.cs
@@ -16,10 +16,15 @@ namespace VeterinarioConsulta.Paginas
 		public Mapa ()
 		{
 			InitializeComponent ();
+            IniciarMapa();
+        }
+
+        private void IniciarMapa()
+        {
             var mapa = new Map(MapSpan.FromCenterAndRadius(new Position(-23.6581925, -46.7829961),
                 Distance.FromKilometers(2)));
             StackMapa.Children.Add(mapa);
             //mapa.MoveToRegion[];
-		}
-	}
+        }
+    }
 }
