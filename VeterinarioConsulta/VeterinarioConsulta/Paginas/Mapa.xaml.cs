fc5972819fa6e8c9221043ec811686a7e4bc0ebe
RodrigoCesarAntunes <ro_sgca@hotmail.com>
2018-12-26 16:34:40 -0200

    Pins adicionados ao mapa

--- VeterinarioConsulta/VeterinarioConsulta/Paginas/Mapa.xaml.cs
+++ VeterinarioConsulta/VeterinarioConsulta/Paginas/Mapa.xaml.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms.Maps;
 
+using Xamarin.Forms.Maps;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+using VeterinarioConsulta.Servicos;
 
 namespace VeterinarioConsulta.Paginas
 {
@@ -27,7 +29,7 @@ namespace VeterinarioConsulta.Paginas
                 Distance.FromKilometers(2)));
             mapa.MapType = MapType.Street;
             StackMapa.Children.Add(mapa);
-            
+            MostrarEstabelecimentos();
             //mapa.MoveToRegion[];
         }
         
@@ -35,6 +37,30 @@ namespace VeterinarioConsulta.Paginas
         {
             base.OnAppearing();
             mapa.IsShowingUser = true;
+        }
+
+        private void MostrarEstabelecimentos()
+        {
+            if (mapa == null)
+                return;
+
+            var enderecoServico = new EnderecosServico();
+            var enderecos = enderecoServico.ObterTodos();
+
+            enderecos.Select(e =>
+                {
+                    var pin = new Pin();
+                    mapa.Pins.Add(new Pin()
+                    {
+                        Position = new Position(e.Latitude, e.Longitude),
+                        Label = "Consultorio 1",
+                        Address = e.Endereco + " " + e.Numero
+                    });
+                    return pin;
+                }
+            ).ToList();
+
+            
         }
     }
 }
--- VeterinarioConsulta/VeterinarioConsulta/Servicos/EnderecosServico.cs
+++ VeterinarioConsulta/VeterinarioConsulta/Servicos/EnderecosServico.cs
@@ -11,10 +11,10 @@ namespace VeterinarioConsulta.Servicos
         {
             var enderecos = new List<Enderecos>()
             {
-                new Enderecos(){ Endereco= "Rua Gêres", Latitude = -23.6592706, Longitude = -46.7812831, Numero = 210},
-                new Enderecos(){ Endereco = "Estr. de Itapecerica", Latitude = -23.6599295, Longitude = -46.7782246, Numero=4830}
+                new Enderecos(){ Endereco= "Rua Gêres", Latitude = -23.65964, Longitude = -46.781793, Numero = 210},
+                new Enderecos(){ Endereco = "Estr. de Itapecerica", Latitude = -23.6592171, Longitude = -46.7744844, Numero=4830},
+                new Enderecos() { Endereco = "R. Pedro José da Silva, 280-398 - Jardim Campo dos Ferreiros, São Paulo - SP", Latitude = -23.658941, Longitude = -46.778368, Numero = 280 }
             };
-
             return enderecos;
         }
     }
