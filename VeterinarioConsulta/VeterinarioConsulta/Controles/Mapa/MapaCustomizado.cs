95ce8675d4eb42aecb9a272872356e849312ba45
RodrigoCesarAntunes <ro_sgca@hotmail.com>
2018-12-26 23:49:53 -0200

    utils e controles adicionados

---
+++ VeterinarioConsulta/VeterinarioConsulta/Controles/Mapa/MapaCustomizado.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Maps;
+
+namespace VeterinarioConsulta.Controles.Mapa
+{
+    public class MapaCustomizado: Map
+    {
+        public MapaCustomizado(MapSpan mapSpan)
+            :base(mapSpan)
+        {
+
+        }
+        public MapaCustomizado()
+        {
+
+        }
+    }
+}
