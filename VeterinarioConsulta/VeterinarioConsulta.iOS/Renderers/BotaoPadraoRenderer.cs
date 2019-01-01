d077a370a3043aace81410e23b14edf8d34c6310
RodrigoCesarAntunes <ro_sgca@hotmail.com>
2019-01-01 10:01:36 -0200

    BotaoPadraoRenderer do IOS adicionado

---
+++ VeterinarioConsulta/VeterinarioConsulta.iOS/Renderers/BotaoPadraoRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace VeterinarioConsulta.iOS.Renderers
+{
+    public class BotaoPadraoRenderer: ButtonRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+
+            if(e.OldElement == null)
+            {
+                Element.CornerRadius = 10;
+                Element.BackgroundColor = Color.White;
+                Element.BorderWidth = 2;
+                Element.BorderColor = Color.Black;
+            }
+        }
+    }
+}
