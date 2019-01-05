0629a10247c417c6dcb2da945887a6e9fb360780
Rodrigo Antunes <ro_sgca@hotmail.com>
2019-01-05 13:56:00 -0200

    Metodo de extenção de parse adicionado no objeto string

---
+++ VeterinarioConsulta/VeterinarioConsulta/Utils/ParseDeString.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VeterinarioConsulta.Utils
+{
+    public static class ParseDeString
+    {
+
+        /// <summary>
+        /// Comverte String para Inteiro sem a necessidade de uma validação
+        /// </summary>
+        /// <param name="inteiro"></param>
+        /// <returns></returns>
+        public static int ToInt(this string inteiro)
+        {
+            int resultado;
+            int.TryParse(inteiro, out resultado);
+            return resultado;
+        }
+
+        /// <summary>
+        /// Comverte String para Double sem a necessidade de uma validação
+        /// </summary>
+        /// <param name="inteiro"></param>
+        /// <returns></returns>
+        public static double ToDouble(this string Double)
+        {
+            double resultado;
+            double.TryParse(Double, out resultado);
+            return resultado;
+        }
+
+        /// <summary>
+        /// Comverte String para Float sem a necessidade de uma validação
+        /// </summary>
+        /// <param name="inteiro"></param>
+        /// <returns></returns>
+        public static float ToFloat(this string Flutuante)
+        {
+            float resultado;
+            float.TryParse(Flutuante, out resultado);
+            return resultado;
+        }
+
+        /// <summary>
+        /// Comverte String para Decimal sem a necessidade de uma validação
+        /// </summary>
+        /// <param name="inteiro"></param>
+        /// <returns></returns>
+        public static decimal ToDecimal(this string Decimal)
+        {
+            decimal resultado;
+            decimal.TryParse(Decimal, out resultado);
+            return resultado;
+        }
+
+        /// <summary>
+        /// Comverte String para Boolean sem a necessidade de uma validação
+        /// </summary>
+        /// <param name="inteiro"></param>
+        /// <returns></returns>
+        public static bool ToBoolean(this string Boolean)
+        {
+            bool resultado;
+            bool.TryParse(Boolean, out resultado);
+            return resultado;
+        }
+
+
+    }
+}
