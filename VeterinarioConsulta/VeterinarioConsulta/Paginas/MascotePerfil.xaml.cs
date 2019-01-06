c98b00201619aae2a73cccd500c446194416936d
Rodrigo Antunes <ro_sgca@hotmail.com>
2019-01-06 15:54:18 -0200

    Modal de fotos de mascotes adicionado

--- VeterinarioConsulta/VeterinarioConsulta/Paginas/MascotePerfil.xaml.cs
+++ VeterinarioConsulta/VeterinarioConsulta/Paginas/MascotePerfil.xaml.cs
@@ -1,33 +1,35 @@
-﻿using System;
+﻿using Rg.Plugins.Popup.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VeterinarioConsulta.Servicos;
+using VeterinarioConsulta.Utils;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace VeterinarioConsulta.Paginas
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MascotePerfil : ContentPage
-	{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MascotePerfil : ContentPage
+    {
 
         private bool isEditar;
 
-        public MascotePerfil ()
-		{
-			InitializeComponent ();
+        public MascotePerfil()
+        {
+            InitializeComponent();
             PreencherPerfil();
-
+            CarregarFotos();
         }
 
-        
+
 
         public void PreencherPerfil()
         {
             var animal = AnimaisServico.AnimalAtual;
-            
+
             editDescricao.Text = animal.Descricao;
             txtEspecie.Text = animal.Especie;
             txtIdade.Text = animal.Idade;
@@ -74,7 +76,7 @@ namespace VeterinarioConsulta.Paginas
             txtPeso.IsEnabled = false;
             txtRaca.IsEnabled = false;
             txtTamanho.IsEnabled = false;
-            
+
             btnToolbar.Text = "Editar";
         }
 
@@ -85,6 +87,51 @@ namespace VeterinarioConsulta.Paginas
             isEditar = false;
         }
 
+        private void CarregarFotos()
+        {
+            var gestureRecognizer = new TapGestureRecognizer();
+            gestureRecognizer.Tapped += AbriImagem;
+            //var Frame = new Frame() { HeightRequest= 20, WidthRequest = 20};
+            var img = new Image() { Source = "womenWithDog.png", HeightRequest = 60, WidthRequest = 65};
+            var img2 = new Image() { Source = "cachorrinho.png", HeightRequest = 60, WidthRequest = 65};
+            var img3 = new Image() { Source = "womenWithDog.png", HeightRequest = 60, WidthRequest = 65 };
+            var img4 = new Image() { Source = "cachorrinho.png", HeightRequest = 60, WidthRequest = 65};
+            var img5 = new Image() { Source = "womenWithDog.png", HeightRequest = 60, WidthRequest = 65 };
+            var img6 = new Image() { Source = "cachorrinho.png", HeightRequest = 60, WidthRequest = 65};
+            var img7 = new Image() { Source = "womenWithDog.png", HeightRequest = 60, WidthRequest = 65 };
+            var img8 = new Image() { Source = "cachorrinho.png", HeightRequest = 60, WidthRequest = 65};
+
+            img.GestureRecognizers.Add(gestureRecognizer);
+            img2.GestureRecognizers.Add(gestureRecognizer);
+            img3.GestureRecognizers.Add(gestureRecognizer);
+            img4.GestureRecognizers.Add(gestureRecognizer);
+            img5.GestureRecognizers.Add(gestureRecognizer);
+            img6.GestureRecognizers.Add(gestureRecognizer);
+            img7.GestureRecognizers.Add(gestureRecognizer);
+            img8.GestureRecognizers.Add(gestureRecognizer);
+            
+            flexFotos.Children.Add(img);
+            flexFotos.Children.Add(img2);
+            flexFotos.Children.Add(img3);
+            flexFotos.Children.Add(img4);
+            flexFotos.Children.Add(img5);
+            flexFotos.Children.Add(img6);
+            flexFotos.Children.Add(img7);
+            flexFotos.Children.Add(img8);
+            //flexFotos.Children.Add(new Frame() { Content = img, HeightRequest=60, WidthRequest=60});
+            //flexFotos.Children.Add(new Frame() { Content = img2, HeightRequest = 60, WidthRequest = 60 });
+            
+        }
+
+
+        private async void AbriImagem(object sender, EventArgs args)
+        {
+            var img = (sender as Image);
+            var Modal = new TratarModal();
+            var source = img.Source as FileImageSource;
+
+            await Modal.AbrirModal(new VisualizarImagens() { imagemSource = source.File });
+        }
 
     }
 }
---
+++ VeterinarioConsulta/VeterinarioConsulta/Paginas/VisualizarImagens.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeterinarioConsulta.Utils;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VeterinarioConsulta.Paginas
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class VisualizarImagens : PopupPage
+	{
+		public VisualizarImagens ()
+		{
+			InitializeComponent ();
+            this.CloseWhenBackgroundIsClicked = true;
+            //pagina.BackgroundClicked += PopupPage_BackgroundClicked;
+        }
+
+        public string imagemSource
+        {
+            set { img.Source = value; }
+        }
+
+        //protected async void PopupPage_BackgroundClicked(object sender, EventArgs e)
+        //{
+        //    var modal = new TratarModal();
+        //    await modal.FecharModal();
+        //}
+    }
+}
